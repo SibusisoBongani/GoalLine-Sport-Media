bdbc3dddf532f81c37070daf33baf721bf0f6b2c
Sibusiso Bongani Maseko <rappsta2004@gmail.com>
2025-07-17 16:11:24 +0200

    Add files via upload
    Added JSON generator script

---
+++ scripts/generate-json.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
+
+// Update this path if your posts are in 'posts' instead of '_posts'
+const postsDir = path.join(__dirname, '../_posts'); 
+const outputFile = path.join(__dirname, '../articles.json');
+
+const articles = fs.readdirSync(postsDir).map(file => {
+  const content = fs.readFileSync(path.join(postsDir, file), 'utf8');
+  const { data, content: body } = matter(content);
+  return { ...data, body };
+});
+
+fs.writeFileSync(outputFile, JSON.stringify(articles));
+console.log('Generated articles.json!');
